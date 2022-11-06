b9e24717c501fef22319278bfee0db50d83f199e
sgaragagghu <sgaragagghu@gmail.com>
2022-11-06 18:52:35 +0100

    changed virtual machine profile, more ram was needed

--- project.tf
+++ project.tf
@@ -14,7 +14,7 @@ provider "aws" {
 resource "aws_instance" "app_instance" {
   ami           = "ami-0c7ed0ec32865e4db"   
 #  ami           = "ami-02538f8925e3aa27a"
-  instance_type = "t2.nano"
+  instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-01352b25182ee7b0b"]
   subnet_id              = "subnet-0cd3e50d7ca772ee0"
   key_name = "SDCC"
